b263eee76fe2f39bbfcf5e7e1762272b2ca9d299
Калистратов Антон Сергеевич <kalistratov@skbkontur.ru>
2018-11-10 18:38:46 +0500

    delete unnecessary methods

--- DataCollectionService/Services/ClientCardDatabaseService.cs
+++ DataCollectionService/Services/ClientCardDatabaseService.cs
@@ -16,32 +16,6 @@ namespace DataCollectionService.Services
             this.options = options;
         }
 
-        public void AddClientCard(ClientCard clientCard)
-        {
-            try
-            {
-                using (var dbContext = new ClientCardContext(this.options))
-                {
-                    dbContext.Add(clientCard);
-                    dbContext.SaveChanges();
-                }
-            }
-            catch (Exception)
-            {
-                throw new ArgumentException("Result Add method is invalid");
-            }
-        }
-
-        public List<ClientCard> GetAllClientCards()
-        {
-            List<ClientCard> clientCards;
-            using (var db = new ClientCardContext(this.options))
-            {
-                clientCards = db.ClientCards.ToList();
-                return clientCards;
-            }
-        }
-
         public void AddClientCardWithContext(ClientCard clientCard, ClientCardContext context)
         {
             var res = context.ClientCards.FirstOrDefault(p => p.ContractId == clientCard.ContractId);
